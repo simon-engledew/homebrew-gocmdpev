911ee3a5fe2fda76f937961893f07f551b4337dc
Simon Engledew <simon@engledew.com>
2016-06-20 11:12:06 +0100

    Update SHA hash

--- gocmdpev.rb
+++ gocmdpev.rb
@@ -2,7 +2,7 @@ class Gocmdpev < Formula
   desc "Command-line GO Postgres query visualizer"
   homepage "https://github.com/simon-engledew/gocmdpev"
   url "https://github.com/simon-engledew/gocmdpev/releases/download/1.0.1/gocmdpev"
-  sha256 "0affabafb68667f01b244842446aaad5d6717a2c14f9aef9faefd59668702e03"
+  sha256 "738506b93ad3537f01651371d4142803cb47659709c2e8fea2fa46c2b17418a7"
 
   def install
     bin.install "gocmdpev"
